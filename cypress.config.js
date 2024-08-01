3035ca523ab289074b596138e0aefff9deab9c82
Arthur Silva <arthursilvamirandanobre@gmail.com>
2024-08-01 19:32:58 -0300

    mudança no gitignore

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "gy8o8u",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
